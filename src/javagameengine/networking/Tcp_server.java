98f576bbab23504cb2bfad535d42fc18017f2422
SharkooMaster <alirashini2323@outlook.com>
2022-06-09 04:11:58 +0200

    Split sockets and server into different classes and enabled multithreading to allow for multiple client communication in a single instance

--- src/javagameengine/networking/Tcp_server.java
+++ src/javagameengine/networking/Tcp_server.java
@@ -9,12 +9,20 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Tcp_server extends Thread {
+class socket_handler implements Runnable {
+    public Thread t;
     public ServerSocket socket;     // Socket to listen on
     public Socket       i_socket;   // Start listening on socket
 
-    public Tcp_server()
+    @Override
+    public void run() {
+
+    }
+
+    public socket_handler(String _lineup_id)
     {
+        t = new Thread(this, "t_client_" + _lineup_id);
+        t.start();
         try {
             socket   = new ServerSocket(8074);
             i_socket = socket.accept();
@@ -43,5 +51,39 @@ public class Tcp_server extends Thread {
             throw new RuntimeException(e);
         }
         return parsed;
+    }
+}
+
+public class Tcp_server extends Thread {
+
+    public Tcp_server()
+    {
+        Tcp_server thread = new Tcp_server();
+        thread.start();
+    }
+
+    // Connected lineup ** List of connected clients **
+    public int lineup_id = 0;
+
+    public String client_joined()
+    {
+        lineup_id = lineup_id++;
+        return Integer.toString(lineup_id);
+    }
+
+    public void client_left()
+    {
+        // We don't handle any queues. Meaning it doesn't matter what the ID is as long as they don't conflict.
+        // So this is just a security measure to make sure that it doesn't happen.
+        lineup_id = lineup_id++;
+    }
+
+    public void listenFor_lineup()
+    {
+
+    }
+
+    public void read_lineup()
+    {
     }
 }
