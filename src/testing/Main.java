65b85ce497571949c6d12dca0dca04a8f9009588
Alfred Roos <alfred@stensatter.se>
2022-10-14 11:37:56 +0200

    scale

--- src/testing/Main.java
+++ src/testing/Main.java
@@ -3,13 +3,7 @@ package testing;
 import javagameengine.JavaGameEngine;
 import javagameengine.Scene;
 import javagameengine.components.*;
-import javagameengine.msc.Padding;
 import javagameengine.msc.Vector2;
-import javagameengine.ui.Button;
-import javagameengine.ui.layout.Column;
-import javagameengine.ui.layout.Panel;
-
-import java.awt.*;
 
 public class Main extends JavaGameEngine {
 
@@ -17,37 +11,7 @@ public class Main extends JavaGameEngine {
 
         Scene scene1 = new Scene();
 
-        Panel twoCols = new Panel();
-        twoCols.setPadding(new Padding(5));
-        twoCols.setColor(Color.red);
-        twoCols.setPosition(new Vector2(100,100));
 
-        Panel col1 = new Panel();
-        col1.setPadding(new Padding(4));
-        Column column = new Column(col1);
-        col1.setLayout(column);
-
-        col1.setColor(Color.green);
-        col1.add(new Button("1"));
-        col1.add(new Button("2"));
-        col1.add(new Button("3"));
-
-
-        Panel col2 = new Panel();
-        col2.setPadding(new Padding(4));
-        Column column1 = new Column(col2);
-        col2.setLayout(column1);
-
-        col2.setColor(Color.blue);
-        col2.add(new Button("4"));
-        col2.add(new Button("5"));
-        col2.add(new Button("6"));
-
-        twoCols.add(col2);
-        twoCols.add(col1);
-        twoCols.add(new Button("sad"));
-
-        scene1.add(twoCols);
 
         scene1.getCamera().add(new CameraMovement());
 
