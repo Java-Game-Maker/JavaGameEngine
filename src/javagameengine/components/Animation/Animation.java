c087eb3f59c6759e216796531467fec70e85f288
Alfred Roos <alfred@stensatter.se>
2022-10-17 21:55:34 +0200

    did not work but i thought it did at first

---
+++ src/javagameengine/components/Animation/Animation.java
@@ -0,0 +1,26 @@
+package javagameengine.components;
+
+import javagameengine.msc.Vector2;
+
+import java.util.LinkedList;
+
+public class Animation extends Component{
+
+    boolean             repeat = false;                 // repeat or not
+    int                 time   =     0;                 // how long the animation is
+    LinkedList<Vector2> points = new LinkedList<>();    // the points to move to
+    LinkedList<Vector2> selectedPoints = new LinkedList<>();    // the points to move to
+
+    public Animation(LinkedList<Vector2> selectedPoints,int time){
+        this.selectedPoints = selectedPoints;
+        this.time = time;
+    }
+    /**
+     * This function generates all the points between the selected
+     * points to create a smooth transition
+     */
+    public void generate(){
+
+    }
+
+}
---
+++ src/javagameengine/components/Animation/AnimationPoint.java
@@ -0,0 +1,2 @@
+package javagameengine.components.Animation;public class AnimationPoint {
+}
