530894cba53feed32cbc21e621c8c1c2a1dff954
Alfred Roos <alfred@stensatter.se>
2022-10-18 19:18:10 +0200

    it works i hop, i dont båga try it tho :P

--- src/testing/Main.java
+++ src/testing/Main.java
@@ -1,57 +1,61 @@
 package testing;
 
+
 import javagameengine.JavaGameEngine;
 import javagameengine.Scene;
 import javagameengine.components.*;
+import javagameengine.components.Animation.Animation;
+import javagameengine.components.Animation.AnimationPoint;
+import javagameengine.msc.Debug;
 import javagameengine.msc.Vector2;
 
 import java.awt.*;
+import java.util.LinkedList;
 
 public class Main extends JavaGameEngine {
 
     public static void main(String[] args){
 
-        Scene scene1 = new Scene(){
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawString(((hasA!=null)?hasA.toString():""),-100,-100);
-            }
-        };
+        Scene scene1 = new Scene();
+        scene1.getCamera().add(new CameraMovement());
 
-
-        GameObject gameObject = new GameObject();
-        gameObject.setPosition(new Vector2(100,100));
-        gameObject.setColor(Color.GREEN);
-        gameObject.add(new GameObject(){
-            @Override
-            public void start() {
-                super.start();
-                setColor(Color.red);
-                setLayer(30);
-                setScale(new Vector2(60,60));
-                setParentOffset(new Vector2(80,80));
-            }
-        });
-
-        scene1.add(gameObject);
-        scene1.add(new GameObject(){
-            @Override
-            public void start() {
-                super.start();
-                setLayer(20);
-            }
-        });
-
+        scene1.add(new Coin());
 
         setSelectedScene(scene1);
-
         JavaGameEngine.size = new Vector2(1920/2,1080/2);
 
         start();
-
-
     }
 
+    static class Coin extends GameObject{
+        Animation a = new Animation();
+        public Coin(){
+
+            LinkedList<AnimationPoint> points = new LinkedList<>();
+            points.add(new AnimationPoint(0,0,0));
+            points.add(new AnimationPoint(0,100,5));
+            points.add(new AnimationPoint(0,50,15));
+            points.add(new AnimationPoint(0,25,25));
+            points.add(new AnimationPoint(0,0,32));
+
+
+            a.setSelectedPoints(points);
+
+            setScale(new Vector2(10,10));
+
+        }
+        int index = 0;
+        @Override
+        public void update() {
+            super.update();
+            Vector2 pos = a.getPoint();
+            Debug.log(pos);
+            Debug.log(a.getPointIndex());
+            setPosition(pos);
+           // setScale(pos.add(10));
+
+
+        }
+    }
 
 }
