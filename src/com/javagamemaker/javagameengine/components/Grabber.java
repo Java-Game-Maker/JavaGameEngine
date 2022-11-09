a01d3609cbe6c07e71618bff3001f164d1b13cea
spynetS <alfred@stensatter.se>
2022-11-09 18:52:48 +0100

    added grabber component

---
+++ src/com/javagamemaker/javagameengine/components/Grabber.java
@@ -0,0 +1,15 @@
+package com.javagamemaker.javagameengine.components;
+
+public class Grabber extends Component{
+
+    Component parent;
+
+    public Grabber(Component parent){
+        this.parent = parent;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+}
