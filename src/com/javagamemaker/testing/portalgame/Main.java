3fe3546961e94fe2e293f5636e64ea7726c31f9d
spynetS <alfred@stensatter.se>
2022-11-09 18:51:13 +0100

    new game

--- src/com/javagamemaker/testing/portalgame/Main.java
+++ src/com/javagamemaker/testing/portalgame/Main.java
@@ -1,4 +1,34 @@
 package com.javagamemaker.testing.portalgame;
 
-public class Main {
+import com.javagamemaker.javagameengine.JavaGameEngine;
+import com.javagamemaker.javagameengine.Scene;
+import com.javagamemaker.javagameengine.components.Collider;
+import com.javagamemaker.javagameengine.components.GameObject;
+import com.javagamemaker.javagameengine.components.PhysicsBody;
+import com.javagamemaker.javagameengine.msc.Vector2;
+
+import java.awt.*;
+
+public class Main extends JavaGameEngine {
+
+
+    public static void main(String[] args){
+        Scene level1 = new Scene();
+
+        GameObject thing = new GameObject();
+        PhysicsBody p = new PhysicsBody();
+        p.velocity = new Vector2(0,2);
+        thing.add(p);
+        Collider c  = new Collider();
+        c.setTrigger(true);
+        thing.add(c);
+
+        level1.add(thing);
+
+        level1.add(new Portal(new Vector2(-200,-200),new Vector2(0,200)));
+        setSelectedScene(level1);
+
+        start();
+    }
+
 }
--- src/com/javagamemaker/testing/portalgame/Portal.java
+++ src/com/javagamemaker/testing/portalgame/Portal.java
@@ -1,4 +1,51 @@
 package com.javagamemaker.testing.portalgame;
 
-public class Portal {
+import com.javagamemaker.javagameengine.CollisionEvent;
+import com.javagamemaker.javagameengine.components.Collider;
+import com.javagamemaker.javagameengine.components.Component;
+import com.javagamemaker.javagameengine.components.GameObject;
+import com.javagamemaker.javagameengine.msc.Debug;
+import com.javagamemaker.javagameengine.msc.Vector2;
+
+public class Portal extends Component {
+
+    PortalPiece portal1 = new PortalPiece(); //entrance
+    PortalPiece portal2 = new PortalPiece(); // exit
+
+    public Portal(Vector2 entrance, Vector2 exit){
+
+        portal1.other = portal2;
+        portal2.other = portal1;
+
+        add(portal1);
+        add(portal2);
+
+        portal1.setPosition(entrance);
+        portal2.setPosition(exit);
+    }
+
+    class PortalPiece extends GameObject{
+        public PortalPiece other;
+        @Override
+        public void start() {
+
+            super.start();
+
+        }
+
+        public PortalPiece(){
+            Collider c1 = new Collider();
+            c1.setTrigger(true);
+            add(c1);
+        }
+
+        @Override
+        protected void onTriggerEnter(CollisionEvent collisionEvent) {
+            super.onTriggerEnter(collisionEvent);
+            Component ob = collisionEvent.getCollider2().getParent();
+            Vector2 newPos = other.getPosition().add(ob.getScale().removeX());
+            ob.setPosition(newPos);
+        }
+    }
+
 }
