e15ed09817a22a3fe5a6e430045439687fc46073
spy <alfred@stensatter.se>
2022-12-02 21:37:00 +0100

    new game

--- src/com/javagamemaker/testing/spel2/Ground.java
+++ src/com/javagamemaker/testing/spel2/Ground.java
@@ -1,4 +1,22 @@
 package com.javagamemaker.testing.spel2;
 
-public class Ground {
+import com.javagamemaker.javagameengine.components.Collider;
+import com.javagamemaker.javagameengine.components.GameObject;
+import com.javagamemaker.javagameengine.components.Grabber;
+import com.javagamemaker.javagameengine.msc.Vector2;
+
+public class Ground extends GameObject {
+
+    public Ground(float width,Vector2 pos){
+       setScale(new Vector2(width,50));
+       setPosition(pos);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if(Main.player.getPosition().getY() < getPosition().getY() && getChild(new Collider())==null){
+           add(new Collider(localVertices));
+        }
+    }
 }
--- src/com/javagamemaker/testing/spel2/Main.java
+++ src/com/javagamemaker/testing/spel2/Main.java
@@ -1,2 +1,18 @@
-package com.javagamemaker.testing.spel2;public class Main {
+package com.javagamemaker.testing.spel2;
+
+import com.javagamemaker.javagameengine.JavaGameEngine;
+import com.javagamemaker.javagameengine.Scene;
+import com.javagamemaker.javagameengine.msc.Vector2;
+
+public class Main extends JavaGameEngine {
+    public static Player player;
+
+    public static void main(String[] args){
+        player = new Player();
+
+        player.setPosition(new Vector2(0,-200));
+
+        setSelectedScene(new Level1());
+        start();
+    }
 }
