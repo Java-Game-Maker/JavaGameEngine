0fbc560ca1fe145945906732abb589b452a9ce22
spy <alfred@stensatter.se>
2023-01-15 23:23:24 +0100

    added tilemode

--- src/com/javagamemaker/javagameengine/components/Tile.java
+++ src/com/javagamemaker/javagameengine/components/Tile.java
@@ -1,2 +1,41 @@
-package com.javagamemaker.javagameengine.components;public class Tile {
+package com.javagamemaker.javagameengine.components;
+
+import com.javagamemaker.javagameengine.JavaGameEngine;
+import com.javagamemaker.javagameengine.msc.Vector2;
+
+import java.util.ArrayList;
+
+public class Tile extends GameObject{
+    @Override
+    public void translate(Vector2 towards) {
+        int width = 1;
+        try{
+            width = ((TileMap)JavaGameEngine.getSelectedScene()).tileWidth;
+        }catch (Exception e){}
+        towards = towards.multiply(width);
+        super.translate(towards);
+    }
+
+    @Override
+    public void setPosition(Vector2 position) {
+        //this.lastPosition = this.position;
+        int width = 1;
+        try{
+            width = ((TileMap)JavaGameEngine.getSelectedScene()).tileWidth;
+        }catch (Exception e){}
+
+        if(getParent()!=null){
+            this.position = position.add(parentOffset.multiply(width*2)).add(rotOffset);
+
+        }else{
+            prevPosition = this.position;
+            this.position = position;
+        }
+        for(Component c : getChildren()){
+            c.setPosition(position);
+        }
+
+        updateVertices();
+
+    }
 }
--- src/com/javagamemaker/javagameengine/components/TileMap.java
+++ src/com/javagamemaker/javagameengine/components/TileMap.java
@@ -1,2 +1,16 @@
-package com.javagamemaker.javagameengine.components;public class TileMap {
+package com.javagamemaker.javagameengine.components;
+
+import com.javagamemaker.javagameengine.Scene;
+import com.javagamemaker.javagameengine.msc.Vector2;
+
+public class TileMap extends Scene {
+
+    public int tileWidth=16;
+
+    @Override
+    public void add(Component component) {
+        component.setScale(new Vector2(tileWidth,tileWidth));
+        component.setPosition(component.getPosition().multiply(tileWidth));
+        super.add(component);
+    }
 }
